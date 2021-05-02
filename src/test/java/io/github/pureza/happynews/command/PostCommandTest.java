8ca49031c8880e8fc69510e4703482c52c1e8a29
shinichi.tokuda <shinichi.tokuda@gmail.com>
2021-05-03 00:32:28 +0900

    Fix Lint warnings. (org.mockito.Matchers in org.mockito has been deprecated)

--- src/test/java/io/github/pureza/happynews/command/PostCommandTest.java
+++ src/test/java/io/github/pureza/happynews/command/PostCommandTest.java
@@ -12,8 +12,8 @@ import java.util.function.Supplier;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 public class PostCommandTest extends AbstractTest {
